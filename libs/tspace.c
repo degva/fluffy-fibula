ca2f9fbd40cf26f70feb2554e1beddc8438c40ec
Camila Galarreta <camila7gc@hotmail.com>
2015-11-10 01:12:06 -0500

    Actualización 01.12am (tengo sueño -.-)

--- libs/tspace.c
+++ libs/tspace.c
@@ -5,5 +5,15 @@ void TSpace_init(TSpace * S) {
     S->tipoDeSpace = 0;
     S->relleno = NULL;
     S->spaces = (TSpace *) malloc(sizeof(TSpace)*6);
-}
+};
 
+void TSpace_render(TSpace * Ts, SDL_Surface * S) {
+  int i;
+  
+  SDL_BlitSurface(Ts->space, NULL, S, NULL);
+  
+  // Show the texts
+  //for (i=0; i<MENU_TOTAL_BUTTONS; i++) {
+  // TButton_render( *(M->buttons+i), F, S);
+  }
+}
